29757bb58102e7c6f5c876170bf75b8ef11dd131
MillerRabin <millerrabin@raintech.su>
2018-07-03 00:05:17 +0300

    First commit

--- sql/tableData.sql
+++ sql/tableData.sql
@@ -16,7 +16,6 @@ create table users (
     skype varchar(160),
     facebook_id bigint,
     sex char(2),
-    groups ltree[] not null,
     password varchar(60) not null,
     location geography(point, 4326),
     locked boolean not null default false,
@@ -27,7 +26,28 @@ create table users (
     mail_code int
 );
 
+create table user_access (
+    id bigserial primary key,
+    r_user uuid not null,
+    access int not null,
+    create_time timestamp with time zone not null default now(),
+    update_time timestamp with time zone not null default now(),
+    details varchar(1000)
+);
 
+
+alter table user_access
+add constraint user_access_user_fk foreign key (r_user)
+references users (id)
+on delete cascade;
+
+alter table user_access
+add constraint user_access_fk foreign key (access)
+references groups (id)
+on delete cascade;
+
+create unique index user_access_user_index on user_access (r_user, access);
+create index user_access_index on user_access (access);
 
 create table auth_log
 (
@@ -38,18 +58,26 @@ create table auth_log
 
 create table groups
 (
-    id serial,
+    id serial primary key,
     name ltree not null,
     rights jsonb,
     private_data jsonb,
     public_data jsonb,
     create_time timestamp with time zone not null default now(),
-    update_time timestamp with time zone not null default now()
+    update_time timestamp with time zone not null default now(),
+    details varchar(1000)
 )
 
+create unique index groups_name on groups (name);
+alter table groups add primary key (id);
 
-insert into groups (name) values ('top.auth.admin');
 
-insert into users (nick_name, email, phone, password, groups) values ('Miller Rabin', 'millerrabin@raintech.su', 89154230004,
-    crypt('ifyouwanttohave', gen_salt('md5')), ARRAY['top.auth.admin', 'top.ci.admin']::ltree[]);
+insert into groups (name) values ('top.ci.admin');
 
+insert into users (nick_name, email, phone, password) values ('Miller Rabin', 'millerrabin@raintech.su', 89154230004,
+    crypt('ifyouwanttohave', gen_salt('md5')));
+
+insert into user_access (r_user, access)
+select u.id, g.id
+from users u, groups g
+where u.email = 'millerrabin@raintech.su' and g.name = 'top.ci.admin';
